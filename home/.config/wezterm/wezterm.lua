0042ea96087c5dbef78ec038b8a33805020b3c7c
Dax Raad <d@ironbay.co>
2025-05-28 09:44:44 -0400

    sync

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -7,5 +7,6 @@ config.font = wezterm.font 'TX-02'
 config.font_size = 14
 config.enable_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
+config.enable_wayland = false
 
 return config
--- home/.screenlayout/screen.sh
+++ home/.screenlayout/screen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xrandr --output HDMI-0 --off --output DP-4 --mode 5120x2160 --pos 0x0 --rotate normal --dpi 96
+xrandr --output HDMI-0 --off --output DP-0 --mode 5120x2160 --pos 0x0 --rotate normal --dpi 96
 # xrandr --setmonitor VIRTUAL-ONE 1280/200x2160/330+0+0 DP-0
 # xrandr --setmonitor VIRTUAL-TWO 3840/600x2160/330+1280+0 none
-xrandr --setmonitor LEFT 1280/200x2160/330+0+0 DP-4
-xrandr --setmonitor RIGHT 3840/600x2160/330+1280+0 DP-4
+xrandr --setmonitor LEFT 1280/200x2160/330+0+0 DP-0
+xrandr --setmonitor RIGHT 3840/600x2160/330+1280+0 DP-0
